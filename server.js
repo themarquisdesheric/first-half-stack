0646239809f8396fe8dcee9199a267947dcc78e5
Yuval Allweil <themarquisdesheric@gmail.com>
2017-04-20 14:00:01 -0700

    http server in place

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('./lib/app');
+
+const server = http.createServer(app);
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`); //eslint-disable-line
+});
